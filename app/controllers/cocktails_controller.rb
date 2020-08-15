f170eb7b827cd66e27da23a51803718ff1c632c0
Kasia Gawelko <kasia.msg@gmail.com>
2020-08-15 20:58:37 +0200

    last changes

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -23,6 +23,6 @@ class CocktailsController < ApplicationController
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :ingredients, :doses)
+    params.require(:cocktail).permit(:name, :ingredients, :doses, :photo)
   end
 end
